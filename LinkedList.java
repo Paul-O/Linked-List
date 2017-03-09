21ced20208bebb2cc42417b4a6bed50c88f2aae8
MSI\SoulExorcist <aragornbarbossa@gmail.com>
2017-03-09 16:59:48 -0500

    blanked out LinkedList main method for later use

--- LinkedList.java
+++ LinkedList.java
@@ -4,14 +4,7 @@ public class LinkedList
   public Node tail = null;
   public static void main(String args[])
   {
-    Node z = new Node("Jimmy");
-    System.out.println(z.getPrev());
-    System.out.println(z.getNext());
-    System.out.println(z.getName());
-    z.setName("mark");
-    System.out.println(z.getPrev());
-    System.out.println(z.getNext());
-    System.out.println(z.getName());
+    
   }
   public LinkedList()
   {
