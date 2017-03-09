9124618fb89d506e20bf0bf9cfe26130a8e18805
MSI\SoulExorcist <aragornbarbossa@gmail.com>
2017-03-09 16:45:35 -0500

    added add method to LinkedList, made variables public

--- LinkedList.java
+++ LinkedList.java
@@ -1,5 +1,25 @@
 public class LinkedList
 {
-  private Node head = null;
-  private Node tail = null;
+  public Node head = null;
+  public Node tail = null;
+  public void add(Node n)
+  {
+    if(head == null)
+    {
+      head = n;
+      tail = n;
+    }
+    else if(head == tail)
+    {
+      tail = n;
+      head.next = tail;
+      tail.prev = head;
+    }
+    else
+    {
+      n.prev = tail;
+      tail.next = n;
+      tail = n;
+    }
+  }
 }
